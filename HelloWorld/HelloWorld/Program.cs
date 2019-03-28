a0802813b90afd0caa81a2730d5cca134ce6fb53
Prerna Agarwal <prerna214@gmail.com>
2019-03-27 23:10:11 -0700

    command line args

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,10 +8,17 @@ namespace HelloWorld
 {
     class Program
     {
+        //Unlike in C++, the array does not include the name of the executable(exe) file.
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Enter any key to exit");
+            Console.WriteLine("Number of arguments: " + args.Length);
+            foreach(string s in args)
+            {
+                Console.WriteLine(s);
+            }
             Console.ReadLine();
         }
     }
